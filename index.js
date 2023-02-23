757e27a4fb4ace21f658483c14b3eddd31238231
Danilo664 <danilomilosevic123@gmail.com>
2023-02-23 16:43:44 +0100

    moved code from JS, and turned it into html stuff and css

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-let container = document.createElement("div")
-container.setAttribute("id", "container")//the container <----------------------------------
-
-document.body.appendChild(container)
-//the sketch portrait
-container.setAttribute("style", "display:grid;width:500px;height:500px;border:2px solid purple;background-color:white")
-document.body.setAttribute("style", "background-color:blue")
-//nice color, nice stuff
-//now time to make responsive stuff, based on height and stuff
-
-let inputStuff = document.createElement("input")
-inputStuff.setAttribute("style", "width:100px")
-document.body.appendChild(inputStuff)
-
-let applyButton = document.createElement("button")
-applyButton.setAttribute("style","width: 50px")
-applyButton.innerText = "Apply";
-document.body.appendChild(applyButton)
-
+let container = document.getElementById("container");
 let sizeInPixels;
+let applyButton = document.getElementById("theButton");
+let inputStuff = document.getElementById("theInput");
 applyButton.addEventListener("click", ()=>{
     let amount = inputStuff.value
     sizeInPixels = 500 / amount;
